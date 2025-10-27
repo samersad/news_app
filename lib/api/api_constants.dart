3bfa13c7d83cd8c06fa3a4f1e759722459495512
???? <samrsd893@gmail.com>
2025-10-27 19:22:23 +0300

    -

--- lib/api/api_constants.dart
+++ lib/api/api_constants.dart
@@ -1,4 +1,4 @@
 class ApiConstants{
   static const String baseUrl="newsapi.org";
-  static const String apiKey="01dc74abbca3400e8ab1d2200d02ea39";
+  static const String apiKey="6c3d95648f21488da14510b0df98ffac";
 }
